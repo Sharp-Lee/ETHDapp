2c37c1ecda8e6c683cc094c63e9ffa8280d28d2c
Sharp-Lee <903016116@qq.com>
2021-01-10 12:47:24 +0800

    add enum

---
+++ enum/Enum1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.6;
+
+import "./EnumDeclaration.sol";
+
+contract Enum {
+    Status public status;
+}
---
+++ enum/EnumDeclaration.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.6;
+
+enum Status {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}

83f70026172fd3cc8b098255480ee2f7271cce4a
Sharp-Lee <903016116@qq.com>
2021-01-16 11:59:53 +0800

    add CallingOtherContract

---
+++ call_other_contract/migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const Callee = artifacts.require("Callee");
+const Caller = artifacts.require("Caller");
+
+module.exports = function (deployer) {
+  deployer.deploy(Callee);
+  deployer.deploy(Caller);
+};

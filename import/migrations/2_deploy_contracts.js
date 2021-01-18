b565d68ef398753b89a6d47068860efee70727d8
Sharp-Lee <903016116@qq.com>
2021-01-18 16:48:56 +0800

    add import

--- import/migrations/2_deploy_contracts.js
+++ import/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Import = artifacts.require("Import");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Import);
 };
